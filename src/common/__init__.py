aa185edac72387fb95b9d9115e49e9929a3ad925
JD Black <black.jondavid@gmail.com>
2025-09-21 06:25:11 -0500

    added stubs for other tools, updated project configuration

---
+++ src/common/__init__.py
@@ -0,0 +1,6 @@
+# from .cli import main
+from common.utils import yaml_tools_version
+
+__all__ = [
+    "yaml_tools_version",
+]
---
+++ tests/yaql/test_yaql.py
@@ -0,0 +1,18 @@
+
+
+import subprocess
+
+
+def run_cli(args):
+    filtered_args = [item for item in args if item is not None]
+    result = subprocess.run(
+        ["yaql"] + filtered_args,
+        capture_output=True,
+        text=True
+    )
+    return result
+
+def test_version_command():
+    result = run_cli(["--version"])
+    assert result.returncode == 0
+    assert "YAQL version" in result.stdout
